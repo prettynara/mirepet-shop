5f5ef068630c99ed45ee9367548c4ba3f7cd7e5d
nara <byeolnara.choe@esprit.tn>
2025-09-09 17:18:47 +0100

    Feat: add Login/SignUp form component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,12 +4,28 @@ const Login = () => {
 
   const [currentState, setCurrentState] =useState('Sign Up');
 
+  const onSubmitHandler = async (event) => {
+      event.preventDefault();
+  }
+
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
         <div className='inline-flex items-center gap-2 mb-2 mt-10'>
           <p className='fredoka-regular text-3xl'>{currentState}</p>
           <hr className='border-non h-[1.5px] w-8 bg-gray-800' />
         </div>
+        {currentState === 'Login' ? '' :<input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required />}
+        <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+        <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Paswword' required />
+        <div className='w-full flex justify-between text-sm mt-[-8px]'>
+          <p className='cursor-pointer'>Forgot your password?</p>
+          {
+            currentState === 'Login'
+            ? <p onClick={()=>setCurrentState('Sing Up')} className='cursor-pointer'>Create account</p>
+            : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+          }
+        </div>
+        <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign up'}</button>
     </form>
   )
 }
