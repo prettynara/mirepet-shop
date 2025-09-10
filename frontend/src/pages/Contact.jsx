e680e1dbd77a905ccf1dd4e1c001e4dd143ecbd8
nara <byeolnara.choe@esprit.tn>
2025-09-10 16:05:52 +0100

    feat: Contact page UI upgrade

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,19 +8,30 @@ const Contact = () => {
   return (
     <div>
       
+      {/* Title */}
       <div className='text-center text-2xl pt-10 border-t'>
         <Title text1={'CONTACT'} text2={'US'} />
       </div>
 
-      <div className='my-2 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+      {/* Content */}
+      <div className='my-0 flex flex-col justify-center md:flex-row gap-10 mb-10'>
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+
+        
         <div className='flex flex-col justify-center items-start gap-6'>
+        {/* Store Info */}
+        <div className="pl-6 border-l-4 border-blue-200">
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className='text-gray-500'>menzah 8 Tunis</p>
           <p className='text-gray-500'>Tel : (216) 51 134 499 <br/> Email : mire.pawpet@gmail.com </p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
-          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-blck px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>  
+
+        {/* Careers */}
+        <div className="pl-6 border-l-4 border-green-200">  
+          <p className='font-semibold text-xl text-gray-700 mb-1'>Careers at Forever</p>
+          <p className='text-gray-500 mb-3'>Learn more about our teams and job openings.</p>
+          <button className='bg-blue-400 hover:bg-blue-700 transition-colors text-white text-sm font-medium px-8 py-3 rounded-lg shadow'>Explore Jobs</button>
+        </div>
         </div>
       </div>
 
