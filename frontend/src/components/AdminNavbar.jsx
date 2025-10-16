d962bbdd248aff4672594a1e13d0655fe29b9f70
nara <byeolnara.choe@esprit.tn>
2025-10-16 17:05:09 +0100

    style: upgrade AdminNavbar UI design with improved layout for Admin penel and logout

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -3,12 +3,20 @@ import { assets } from './../assets/assets';
 
 const AdminNavbar = () => {
   return (
-    <div>
-      {/* Logo */}
-      <img src={assets.logo} className='w-36' alt="" />
-      <button>Logout</button>
+    <div className="flex items-center justify-between px-6 py-4 bg-white shadow-sm">
+           
+      <div className="flex items-center gap-4">
+        <img src={assets.logo} className="w-36" alt="Logo" />
+        <h1 className="inline-flex items-center gap-2 bg-[#2563EB] text-white font-medium px-4 py-1.5 rounded-full shadow-sm text-lg">
+          Admin Panel
+        </h1>
+      </div>
+   
+      <button className="bg-blue-500 hover:bg-blue-600 transition-colors text-white px-6 py-2 rounded-full shadow-sm text-sm font-medium">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AdminNavbar
+export default AdminNavbar;
